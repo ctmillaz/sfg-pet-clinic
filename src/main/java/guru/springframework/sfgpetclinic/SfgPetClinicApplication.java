c9afcb96bd003ce5f868231a6ee95759c115e6a6
Charles Miller <Charles.Miller@kratosdefense.com>
2018-08-06 15:12:07 -0400

    adding POJOs. Closes #2.

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -1,9 +1,5 @@
 package guru.springframework.sfgpetclinic;
 
-import guru.springframework.sfgpetclinic.controllers.ConstructorInjectedController;
-import guru.springframework.sfgpetclinic.controllers.MyController;
-import guru.springframework.sfgpetclinic.controllers.PropertyInjectedController;
-import guru.springframework.sfgpetclinic.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,13 +9,6 @@ public class SfgPetClinicApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx =SpringApplication.run(SfgPetClinicApplication.class, args);
-
-		MyController controller = (MyController) ctx.getBean("myController");
-
-        System.out.println(controller.hello());
-		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 
 	}
 }
--- src/main/java/guru/springframework/sfgpetclinic/services/GreetingService.java
+++
@@ -1,6 +0,0 @@
-package guru.springframework.sfgpetclinic.services;
-
-public interface GreetingService {
-
-    String sayGreeting();
-}
